09e6d511f840d5e077b029a22e6043e67717817d
Robertishimwe <81079680+Robertishimwe@users.noreply.github.com>
2024-10-06 21:06:03 +0000

    over

--- fronend2/src/utils/api.jsx
+++ fronend2/src/utils/api.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const headers = {
-    "Access-Control-Allow-Origin": "https://jubilant-disco-7x45v6vgq973rj9q-5173.app.github.dev"
-};
+const headers = {};
 
 export default axios.create({  
   baseURL: "https://kb-3.onrender.com",
